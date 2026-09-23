cd5ce1586eabe5f3b195392c69b83581e34a8668
Juanestebann <juanestebann14@gmail.com>
2026-09-23 20:17:02 -0300

    Mover vista desde ui.theme a ui.screens

--- app/src/main/java/com/example/bebederosapp/agrobebederos/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/bebederosapp/agrobebederos/ui/screens/HomeScreen.kt
@@ -1,7 +1,12 @@
-package com.example.bebederosapp.agrobebederos.ui.theme
+package com.example.bebederosapp.agrobebederos.ui.screens
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
